573466bc45cd6279e56dc50da269d7953984ffb9
Rahul Modi <rahmodi@gmail.com>
2014-03-06 22:01:56 -0500

    Add Xcode Project

---
+++ DataStructures/DataStructures/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Rahul Modi on 3/6/14.
+//  Copyright (c) 2014 Rahul Modi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
